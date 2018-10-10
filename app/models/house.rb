3275f5e5b9bfc26b42238cc64b30b1dd58eba02d
Dan Goslen <dgoslen@bandwidth.com>
2018-10-10 19:36:01 -0400

    Fixing formate to format

--- app/models/house.rb
+++ app/models/house.rb
@@ -12,7 +12,7 @@ class House < ApplicationRecord
   validates :has_basement, presence: true, inclusion: {in: [true, false]}
   validates :owner, presence: true, format: { with: /\A[a-zA-Z]+(?: [a-zA-Z]+)?\z/, message: "name format is incorrect" }
   validates :phone, presence: true, length: {is: 10, message: "no dashes"}, numericality: {only_integer: true, message: "only numbers" }
-  validates :email, presence: true, formate: {with: Devise::email_regexp, message: "email format is incorrect" }
+  validates :email, presence: true, format: {with: Devise::email_regexp, message: "email format is incorrect" }
 
   has_one_attached :image_path
 end
